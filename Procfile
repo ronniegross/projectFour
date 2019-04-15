d3b19b1dd85e8338f93341ffbcb7d565b2caf607
Ronnie Gross <rnngrss1495@gmail.co>
2019-04-15 15:20:26 -0400

    changed django settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn asl_project.wsgi --log-file -
--- asl_project/settings.py
+++ asl_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_extensions',
+    'asl_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'asl_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'asl_database',
+        'USER': 'rigbylewis',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
@@ -118,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+import django_heroku
+django_heroku.settings(locals())
