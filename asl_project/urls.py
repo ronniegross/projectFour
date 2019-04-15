757ee731c5df8768111ca70274a11d5192b95a26
Ronnie Gross <rnngrss1495@gmail.co>
2019-04-15 15:42:45 -0400

    fixed urls.py

--- asl_project/urls.py
+++ asl_project/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.FrontendAppView.as_view()),  # New URL for the index route
-    path('api/v1/', include('tunr_app.urls')),
+    path('api/v1/', include('asl_app.urls')),
 ]
