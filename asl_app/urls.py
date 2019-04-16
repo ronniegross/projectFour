a49ab8eb372019c00cb60fbd8cb7fa3d944d999e
Ronnie Gross <rnngrss1495@gmail.co>
2019-04-16 12:27:19 -0400

    set up urls.py

--- asl_app/urls.py
+++ asl_app/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('resources', views.ResourceView)
+router.register('users', views.UserView)
+router.register('comments', views.CommentView)
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
