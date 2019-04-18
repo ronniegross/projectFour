17cee80eb787ce489c6dcc94ab406d32057db91b
Ronnie Gross <rnngrss1495@gmail.co>
2019-04-18 15:58:09 -0400

    add and create user all on user accounts page

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -2,25 +2,34 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import SingleUser from './SingleUser'
+import { Redirect } from 'react-router-dom'
 
 class Users extends Component {
     state = {
-        error: '',
+        user: {
+            name: '',
+            email: '',
+            password: '',
+            age: '',
+            gender: ''
+        },
+        createdUser: {},
+        redirectToHome: false,
         users: []
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.fetchUsers();
     }
 
     fetchUsers = async () => {
         try {
             const res = await axios.get('/api/users/');
-            this.setState({users: res.data});
+            this.setState({ users: res.data });
         }
         catch (err) {
             console.log(err)
-            this.setState({error: err.message})
+            this.setState({ error: err.message })
         }
     }
 
@@ -39,16 +48,37 @@ class Users extends Component {
         }
     }
 
+    createUser = () => {
+        axios.post('/api/users/', this.state.createdUser)
+            // .then(res => {
+                // this.setState({ redirectToHome: true, createdUser: res.data })
+            // })
+    }
+
+    handleSignUp = (event) => {
+        event.preventDefault()
+        this.createUser()
+    }
+
+    handleChange = (event) => {
+        const clonedCreatedUser = { ...this.state.createdUser }
+        clonedCreatedUser[event.target.name] = event.target.value
+        this.setState({ createdUser: clonedCreatedUser })
+    }
+
     render() {
         // console.log(this.state.users.id)
-        if (this.state.error){
-            return <div>{this.state.error}</div>
-        }
+        // if (this.state.error) {
+        //     return <div>{this.state.error}</div>
+        // }
+        // if (this.state.redirectToHome === true && this.state.createdUser.id !== null) {
+        //     return (<Redirect to={'/'} />)
+        // }
         return (
             <div>
                 <h1>All Users</h1>
                 {this.state.users.map(user => (
-                   
+
                     <div key={user.id}>
                         {/* <Link to={`/users/${user.id}`} >{user.name}: {user.id}</Link> */}
                         <Link to={`/user/${user.id}`} >{user.name}: {user.id}</Link>
@@ -56,6 +86,50 @@ class Users extends Component {
                         {/* <button onClick={this.deleteUser}>delete user</button> */}
                     </div>
                 ))}
+                <h2>Create User</h2>
+                <form onSubmit={this.handleSignUp}>
+                    <label htmlFor="name">Full Name</label>
+                    <input
+                        id="name"
+                        type="text"
+                        name="name"
+                        onChange={this.handleChange}
+                        value={this.state.createdUser.name}
+                    />
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="text"
+                        name="email"
+                        onChange={this.handleChange}
+                        value={this.state.createdUser.email}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="text"
+                        name="password"
+                        onChange={this.handleChange}
+                        value={this.state.createdUser.password}
+                    />
+                    <label htmlFor="age">Age</label>
+                    <input
+                        id="age"
+                        type="text"
+                        name="age"
+                        onChange={this.handleChange}
+                        value={this.state.createdUser.age}
+                    />
+                    <label htmlFor="gender">Gender</label>
+                    <input
+                        id="gender"
+                        type="text"
+                        name="gender"
+                        onChange={this.handleChange}
+                        value={this.state.createdUser.gender}
+                    />
+                    <button type="submit">Create Account</button>
+                </form>
             </div>
         );
     }
